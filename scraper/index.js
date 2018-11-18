cd95265aba47cff0aa24728975b86bd03df01927
Jack Davies <mrjackkefford@gmail.com>
2018-11-18 20:54:39 +0000

    Improved

--- scraper/index.js
+++ scraper/index.js
@@ -3,15 +3,15 @@ const axios = require('axios')
 scrape("0");
 
   function scrape(nextId){
-      console.log(nextId);
     axios.get('http://www.pathofexile.com/api/public-stash-tabs?id='+nextId)
     .then(function (response) {
       for (var i = 0; i < response.data.stashes.length; i++) {
           const stash = response.data.stashes[i];
           for (var y = 0; y < stash.items.length; y++) {
-              if(stash.items[y].frameType == 5){
-                  if(stash.items[y].note !== undefined){
-                    console.log(JSON.stringify(stash.items[y]))
+              const item = stash.items[y];
+              if(item.frameType == 5){
+                  if(item.note !== undefined){
+                    console.log(item.league + " " + item.typeLine + " "+ item.stackSize +" "+ item.note + " " + item.id)
                   }
               }   
             }
